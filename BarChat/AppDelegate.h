fd02eabcfe8bd8cecf9aa7f51fc8b9f7bee9adfd
VinceeeWang <vincewang1995@outlook.com>
2017-07-10 12:50:07 +0800

    Initial Commit

---
+++ BarChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BarChat
+//
+//  Created by Vince on 7/10/17.
+//  Copyright © 2017 ChenghaoWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BarChat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BarChat
+//
+//  Created by Vince on 7/10/17.
+//  Copyright © 2017 ChenghaoWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
